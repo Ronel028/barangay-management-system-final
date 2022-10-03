837ba59870e1e9f742db7239668696aece0114c9
Ronel028 <shujihanma028@gmail.com>
2022-10-03 08:46:48 -0700

    initial commit

---
+++ front-end/src/app.js
@@ -0,0 +1,11 @@
+import Login from "./components/login/login";
+
+function App(){
+    return (
+        <>
+            <Login />
+        </>
+    )
+}
+
+export default App;
---
+++ front-end/src/components/login/login.jsx
@@ -0,0 +1,5 @@
+function Login(){
+    return <h1 className="text-primary">This is login page</h1>
+}
+
+export default Login;
